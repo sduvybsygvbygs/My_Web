875e20a2673eead6414c195f7a945d1a51793c53
ARTEMII PYLYPENKO <artemii261204@gmail.com>
2025-10-15 21:16:08 +0300

    Lab6: layout + partials

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url'
+import handlebars from 'vite-plugin-handlebars'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname  = dirname(__filename)
 
 export default defineConfig({
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, 'src', 'partials'),
+            context: { siteName: 'Лабораторна 6', labName: 'Лабораторна № 6' },
+            reloadOnPartialChange: true
+        }),
+    ],
     resolve: { alias: { '@': resolve(__dirname, 'src') } },
     build: {
         rollupOptions: {
